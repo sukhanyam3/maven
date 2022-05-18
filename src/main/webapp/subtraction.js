5a3f23fa96fc84752e17548187586a8f759020ed
sukhanya <sukhanyam3@gmail.com>
2022-05-18 12:03:53 +0530

    n1

---
+++ src/main/webapp/subtraction.js
@@ -0,0 +1,8 @@
+function subtraction(){
+var a = document.getElementById("firstno").value
+var b = document.getElementById("secondno").value
+var result
+result = a-b
+document.getElementById("result").value = result
+}
+
---
+++ target/demo/addition.js
@@ -0,0 +1,9 @@
+function add()
+{
+var a=parseInt(document.getElementById("firstno").value)
+var b=parseInt(document.getElementById("secondno").value)
+var result
+result=a + b
+document.getElementById("result").value=result
+}
+
